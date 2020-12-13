706d2202fefaca649c9932e365c1447b3c51e2eb
Noah Biederbeck <nbiederbeck@tutanota.com>
2020-12-13 11:16:42 +0100

    Move reading of session-token into function call

--- puzzles.py
+++ puzzles.py
@@ -1,14 +1,13 @@
 """Puzzles for Advent of Code 2020."""
-import requests
 from pathlib import Path
-
-with open(Path("~/.config/advent-of-code/token").expanduser()) as f:
-    session = f.read().strip()
+import requests
 
 url = "http://adventofcode.com/2020/day/{}/input"
 
 
 def get_puzzle(day: int):
+    with open(Path("~/.config/advent-of-code/token").expanduser()) as f:
+        session = f.read().strip()
 
     r = requests.get(url.format(day), cookies={"session": session})
     r.raise_for_status()
